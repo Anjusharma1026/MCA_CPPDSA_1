c75f6f9ecc2d01aa2cc9fc5d44fcf72aafcb1c78
Anjusharma1026 <anjusharmaknp34@gmail.com>
2024-06-14 11:48:13 +0530

    f

---
+++ poly.cpp
@@ -0,0 +1,32 @@
+// polymorphism means more than one form 
+//poly-many,morph - forms
+
+#include<iostream>
+using namespace std;
+
+class Count{
+    private:
+    int value;
+
+    public:
+    Count(){
+        value = 5;
+    }
+    
+    void operator ++(){
+        value +=10;
+
+    }
+    void display(){
+        cout<<"Count :" << value <<endl;
+    }
+};
+
+int main(){
+
+Count c1;
+c1.display();
+++c1;
+
+c1.display();
+}
